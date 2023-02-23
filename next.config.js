e450a79546cd4f0cb274ae0df67ed0d52c4bec2b
cairon <egorfrolov2019.ef@gmail.com>
2023-02-24 02:48:04 +0300

    fixs

--- next.config.js
+++ next.config.js
@@ -5,4 +5,12 @@ module.exports = {
     serverRuntimeConfig: {
         PROJECT_ROOT: path.join(__dirname, 'src'),
     },
+    exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+        return {
+            '/': { page: '/' },
+        };
+    },
+    images: {
+        unoptimized: true
+    }
 };
