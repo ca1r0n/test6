f46a4da455035a865e07ebbb02dcb6891f93326b
cairon <egorfrolov2019.ef@gmail.com>
2023-02-24 03:27:25 +0300

    fixs

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@ const path = require('path');
 
 module.exports = {
     basePath: '/test6',
-    exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-        return {
-            '/test6': { page: '/' },
-        };
-    },
-    images: {
-        loader: 'akamai',
-        path: '/test6',
-    },
 };
