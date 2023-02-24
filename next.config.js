f823f781c644c605d4387568e83630e39faf24b9
cairon <egorfrolov2019.ef@gmail.com>
2023-02-24 03:25:31 +0300

    fixs

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     basePath: '/test6',
     exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
         return {
-            '/test6': { page: '/test6' },
+            '/test6': { page: '/' },
         };
     },
     images: {
